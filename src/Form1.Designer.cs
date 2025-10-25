125d268a66c3487d112f9d8b1b0e6074f4952eee
Mantounio <mahdidev83@gmail.com>
2025-10-25 15:40:56 +0330

    changed the size of window and it can't get resized and maximized

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Inventory
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(391, 347);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "MainWindow";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Inventory (version alpha)";
+            ResumeLayout(false);
         }
 
         #endregion
